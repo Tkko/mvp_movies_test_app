906ceab2979e67e8214947b6a3fb5f9e7d948fa9
Tornike Kurdadze <qurdadzze@gmail.com>
2021-09-10 21:38:50 +0400

    Clean up

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -2,7 +2,4 @@ class AppRoutes {
   static const SPLASH = '/splash';
   static const ERROR = '/error';
   static const HOME = '/home';
-  static const AUTH = '/auth';
-  static const SIGN_IN = '/sign_in';
-  static const SIGN_UP = '/sign_up';
 }
